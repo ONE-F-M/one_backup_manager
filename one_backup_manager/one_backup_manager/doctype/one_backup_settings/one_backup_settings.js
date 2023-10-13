67467b9d8b8040170adf7fda322c0037e8ea1277
Ebuka <ebukaakeru@gmail.com>
2023-10-13 23:36:28 +0100

    Added doctypes

---
+++ one_backup_manager/one_backup_manager/doctype/one_backup_settings/one_backup_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, ONE FM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('One Backup Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
