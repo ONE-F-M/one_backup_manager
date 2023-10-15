1b8643b664eda9163bd5accdb4972ab0304e7ec1
Ebuka <ebukaakeru@gmail.com>
2023-10-15 17:51:32 +0100

    Added Logs

--- one_backup_manager/utils/gdrive.py
+++ one_backup_manager/utils/gdrive.py
@@ -1,6 +1,7 @@
 import frappe
 from google.oauth2 import service_account
 import os,io
+from datetime import datetime
 from frappe.utils import cstr
 from one_fm.processor import sendemail
 from googleapiclient.discovery import build
@@ -32,49 +33,96 @@ class GoogleDriveUploader():
             recipient = frappe.conf.error_report_email
             if not recipient:
                 frappe.throw("No Email Recipient Set")
-        print("SENDING MAIL O")
         sendemail(recipients=[recipient],subject = f"Backup {success_value}!",message = message )
-    
-    def upload_file(self,notify_success = False,with_files = False):
-        files = get_latest_files(with_files = with_files)
         
+    def fetch_folder_id(self):
         folder_name = self.settings_doc.backup_folder_name
-        folder_exists = self.drive_api.files().list(q=f"name='{folder_name}'").execute()['files']
-        if folder_exists:
-            folder = folder_exists[0]
-        else:
-            folder_metadata = {
-            "name": folder_name,
-            "mimeType": "application/vnd.google-apps.folder"
-                }
-            try:
-                folder = self.drive_api.files().create(body=folder_metadata, fields="id").execute()
-                permission = {'type': 'user','role': 'reader','emailAddress': self.settings_doc.shared_email_address}
-                self.drive_api.permissions().create(fileId=folder.get('id'), body=permission).execute()
-            except:
-                print("FAILED CREATING FOLDER")
-                frappe.log_error(message = frappe.get_traceback(),title="Error Creating Backup Folder")
+        if folder_name:
+            folder_exists = self.drive_api.files().list(q=f"name='{folder_name}'").execute()['files']
+            if folder_exists:
+                folder = folder_exists[0]
+            else:
+                folder_metadata = {
+                "name": folder_name,
+                "mimeType": "application/vnd.google-apps.folder"
+                    }
+                try:
+                    folder = self.drive_api.files().create(body=folder_metadata, fields="id").execute()
+                    permission = {'type': 'user','role': 'writer','emailAddress': self.settings_doc.shared_email_address}
+                    self.drive_api.permissions().create(fileId=folder.get('id'), body=permission).execute()
+                except:
+                    print("FAILED CREATING FOLDER")
+                    frappe.log_error(message = frappe.get_traceback(),title="Error Creating Backup Folder")
+            return folder
+        
+    def  validate_limit(self,parent_folder_id =None):
+        if not parent_folder_id:
+            parent_folder_id = self.fetch_folder_id()
+        if  self.settings_doc.enable_google_backups:
+            limit = self.settings_doc.number_of_backups
+            if not limit:
+                limit = 3
+            #list the folder in the root backup location
+            
+            sub_folders = self.drive_api.files().list(q=f"'{parent_folder_id.get('id')}' in parents and mimeType = 'application/vnd.google-apps.folder'",orderBy="createdTime desc").execute()['files']
+            
+            if len(sub_folders) > (limit-1):
+                removed_folders = sub_folders[(limit-1):]
+                for folder in removed_folders:
+                    self.drive_api.files().delete(fileId=folder['id']).execute()
+                print(f"Cleared {len(removed_folders)} Folders")
+            else:
+                print("Current Folder Count Below Limit")
+
+    def create_log(self,complete,url=None):
+        """Create a  Backup Log to reflect the status of the upload"""
+        data = frappe._dict({
+            'date':frappe.utils.getdate(),
+            'doctype':'Backup Log',
+            'status':'Completed' if complete else "Failed"
+        })    
+        if not complete:
+            data.error_message = frappe.get_traceback()
+        log_doc = frappe.get_doc(data)
+        if complete:
+            log_doc.uploaded_files = self.settings_doc.backup_with_files
+            log_doc.route = url
+            print("Set Options")
+        log_doc.insert()
+        frappe.db.commit()
+    
+    
+    def upload_file(self):
+        files = get_latest_files(with_files = self.settings_doc.backup_with_files)
+        folder = self.fetch_folder_id()
+        first_file = files.get('files')[0]
+        file_folder_metadata = {
+                    "name": first_file.split('-')[0],
+                    "mimeType": "application/vnd.google-apps.folder",
+                    "parents": [folder.get('id')]
+                        }
+        self.validate_limit(parent_folder_id = folder)
+        filefolder = self.drive_api.files().create(body=file_folder_metadata, fields="id,webViewLink").execute()
+        permission = {'type': 'user','role': 'writer','emailAddress': self.settings_doc.shared_email_address}
+        self.drive_api.permissions().create(fileId=filefolder.get('id'), body=permission).execute()
         if files:
             try:
                 for each in files.get('files'):
-                    file_folder_metadata = {
-                    "name": each.split('.')[0],
-                    "mimeType": "application/vnd.google-apps.folder"
-                        }
-                    filefolder = self.drive_api.files().create(body=file_folder_metadata, fields="id").execute()
-                    permission = {'type': 'user','role': 'reader','emailAddress': self.settings_doc.shared_email_address}
-                    self.drive_api.permissions().create(fileId=filefolder.get('id'), body=permission).execute()
                     file_metadata = {
                         'name': each,
                         "parents": [filefolder.get('id')]
                     }
                     media = MediaFileUpload(files.get('path')+each, resumable=True)
                     file_upload = self.drive_api.files().create(body=file_metadata, media_body=media, fields='id').execute()
-                print("DONE UPLOADING")
+                    frappe.throw("ONE ERROR")
+                
+                self.create_log(1,url =filefolder.get('webViewLink'))
                 if self.settings_doc.send_email_notifications_for_successful_backups:
                     self.notify(success = True,message = f"Please note that the backup of {frappe.local.site} is successful")
-
+                print("DONE UPLOADING")
             except:
-                print("FAILED UPLOADING")
+                print("ERROR OCCURED")
+                print(frappe.get_traceback())
                 frappe.log_error(message = frappe.get_traceback(),title="Error Creating Backup")
                 self.notify(success= False,message = "<p>Please see error log from backup</p><br/>"+frappe.get_traceback())
+                self.create_log(0)
--- one_backup_manager/utils/utils.py
+++ one_backup_manager/utils/utils.py
@@ -1,19 +1,25 @@
 import frappe
 import os
+from frappe.utils.backups import fetch_latest_backups
 
-
+def get_last_batch():
+    return fetch_latest_backups()
+        
 
 def get_latest_files(with_files = 0):
     """fetches the last created file based on frappe's naming culture"""
-    print(os.getcwd())
-    backup_location =  os.getcwd()+"/"+frappe.utils.cstr(frappe.local.site)+'/private/backups/'
-    if 'sites' in os.getcwd():
-        print(os.getcwd())
-        os.chdir(backup_location)
-    else:
-        os.chdir('sites')
-        os.chdir(backup_location)
-    files = os.listdir()
-    files.sort(key=lambda x: os.path.getctime(os.path.join(x)))
-    return {"path":backup_location,'files':files[-1:] if not with_files else files[-3:]}
+    try:
+        files = []
+        last_backup = get_last_batch()
+        if not with_files:
+            del last_backup['private']
+            del last_backup['public']
+        for each in last_backup:
+            #Pick the file name only
+            files.append(last_backup[each].split('/')[-1])
+        backup_location =  os.getcwd()+"/"+frappe.utils.cstr(frappe.local.site)+'/private/backups/'
         
+        return {"path":backup_location,'files':files}
+    except:
+        frappe.throw('Error Fetch Latest Backup')
+        frappe.log_error(title = "Error Fetching Latest Backup files",message= frappe.get_traceback())
